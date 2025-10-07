0adcd7b137c1695607ed0bfaf1f6e987df596e4d
parkhj2009 <eee0110010100@gmail.com>
2025-10-07 21:12:41 +0900

    초기 커밋

--- app/src/main/java/com/example/dodum_android/MainActivity.kt
+++ app/src/main/java/com/example/dodum_android/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.dodum_android.profile.ProfileScreen
 import com.example.dodum_android.ui.theme.Dodum_AndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +19,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
         }
     }
 }
