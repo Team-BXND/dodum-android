10165a32d6da7f600171647ad213f5bbd8f9361e
rtxic011 <kyukami7@gmail.com>
2025-10-29 18:11:00 +0900

    add email send, check service

---
+++ app/src/main/java/com/example/dodum_android/feature/start/signup/VerifyEmailScreen.kt
@@ -0,0 +1,15 @@
+package com.example.dodum_android.feature.start.signup
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.navigation.NavHostController
+
+@Composable
+fun VerifyEmailScreen (
+    navController: NavHostController
+) {
+    val signupViewModel: SignupViewModel = hiltViewModel()
+
+
+
+}
---
+++ app/src/main/java/com/example/dodum_android/network/start/email/EmailService.kt
@@ -0,0 +1,22 @@
+package com.example.dodum_android.network.start.email
+
+import com.example.dodum_android.remote.DodumUrl
+
+package com.example.dodum_android.network.start.email
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface EmailService {
+
+    @POST(DodumUrl.Email.SEND)
+    suspend fun sendEmail(
+        @Body request: EmailSendRequest
+    ): Response<EmailSendSuccessResponse>
+
+    @POST(DodumUrl.Email.CHECK)
+    suspend fun checkEmail(
+        @Body request: EmailCheckRequest
+    ): Response<EmailSendSuccessResponse>
+}
