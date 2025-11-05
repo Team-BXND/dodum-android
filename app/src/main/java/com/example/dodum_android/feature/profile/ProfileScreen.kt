12087da68a8eeb6de66519526bc6313c90d9ec26
parkhj2009 <eee0110010100@gmail.com>
2025-11-05 19:59:23 +0900

    feat: 아이콘 추가

--- app/src/main/java/com/example/dodum_android/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/dodum_android/feature/profile/ProfileScreen.kt
@@ -12,6 +12,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Chat
+import androidx.compose.material.icons.filled.Archive
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
