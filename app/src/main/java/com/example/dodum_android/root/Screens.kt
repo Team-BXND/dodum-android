59dc1846f8aef7da8168096fae11175eaf4733b4
parkhj2009 <eee0110010100@gmail.com>
2025-11-12 17:50:50 +0900

    프로필, 내가 쓴 글 추가

--- app/src/main/java/com/example/dodum_android/root/Screens.kt
+++ app/src/main/java/com/example/dodum_android/root/Screens.kt
@@ -4,4 +4,6 @@ sealed class Screens(val route: String) {
     object MyInform : Screens("myinform")
     object ChangeInform : Screens("changeinform")
     object ChangePw : Screens("changepw")
+    object Profile: Screens("profile")
+    object MyPosts: Screens("myposts")
 }
