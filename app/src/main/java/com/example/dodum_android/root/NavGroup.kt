c0954a58e81e98afa75453c7e0901f9f6039d240
parkhj2009 <eee0110010100@gmail.com>
2025-11-18 21:22:15 +0900

    NavGroup으로 수정

---
+++ app/src/main/java/com/example/dodum_android/root/NavGroup.kt
@@ -0,0 +1,9 @@
+package com.example.dodum_android.root
+
+object NavGroup {
+    const val MyInform = "myinform"
+    const val ChangeInform =  "changeinform"
+    const val ChangePw = "changepw"
+    const val Profile = "profile"
+    const val MyPosts = "myposts"
+}
--- app/src/main/java/com/example/dodum_android/root/Screens.kt
+++
@@ -1,9 +0,0 @@
-package com.example.dodum_android.root
-
-sealed class Screens(val route: String) {
-    object MyInform : Screens("myinform")
-    object ChangeInform : Screens("changeinform")
-    object ChangePw : Screens("changepw")
-    object Profile: Screens("profile")
-    object MyPosts: Screens("myposts")
-}
