27a20a8bbbf6924c75f02f78cff5a87601c71241
parkhj2009 <eee0110010100@gmail.com>
2025-11-28 13:46:04 +0900

    전공 Response 변경

--- app/src/main/java/com/example/dodum_android/network/major/MajorRecommendResponse.kt
+++ app/src/main/java/com/example/dodum_android/network/major/MajorRecommendResponse.kt
@@ -1,8 +1,18 @@
 package com.example.dodum_android.network.major
 
 data class MajorRecommendResponse(
-    val name: String,
-    val description: String,
-    val reason: List<String>,
-    val aptitudes: List<Float>,
+    val major : String,
+    val majorkey : String,
+    val SelectedReason : String,
+    val graph : Graph
+)
+
+
+data class Graph(
+    val web: Int,
+    val server: Int,
+    val game: Int,
+    val iOS: Int,
+    val android: Int,
+    val ai: Int
 )
