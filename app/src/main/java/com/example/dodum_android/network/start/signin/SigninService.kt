72cd96b5449b52b7d5cf9c8ea829d2862da74807
rtxic011 <kyukami7@gmail.com>
2025-12-17 23:16:58 +0900

    add refresh token 재발급 받는 코듣

--- app/src/main/java/com/example/dodum_android/network/start/signin/SigninService.kt
+++ app/src/main/java/com/example/dodum_android/network/start/signin/SigninService.kt
@@ -1,6 +1,7 @@
 package com.example.dodum_android.network.start.signin
 
 import com.example.dodum_android.network.DodumUrl
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -9,5 +10,7 @@ interface SigninService {
     suspend fun signin(@Body request: SigninRequest): SigninResponse
 
     @POST(DodumUrl.Auth.REFRESH)
-    suspend fun refreshToken(@Body request: RefreshTokenRequest): RefreshTokenResponse
+    suspend fun refreshToken(
+        @Body request: RefreshTokenRequest
+    ): Response<SigninResponse>
 }
