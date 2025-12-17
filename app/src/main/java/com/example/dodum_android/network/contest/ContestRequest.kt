7a3bcd17ac5515bcd576156ba73b56c8aa21ba13
rtxic011 <kyukami7@gmail.com>
2025-12-17 22:08:57 +0900

    fix Contest network코드 api 명세서 대로 한 번 더 체크

--- app/src/main/java/com/example/dodum_android/network/contest/ContestRequest.kt
+++ app/src/main/java/com/example/dodum_android/network/contest/ContestRequest.kt
@@ -2,13 +2,13 @@ package com.example.dodum_android.network.contest
 
 data class ContestCreateRequest(
     val title: String,
-    val subtitle: String, // API 명세에는 있으나 피그마 UI에는 안보임, 일단 포함
+    val subtitle: String, // 명세서 포함
     val content: String,
     val phone: String,
     val email: String,
-    val time: String,
+    val time: String, // Date -> String
     val place: String,
-    val image: String
+    val image: String // 명세서엔 없지만 UI/로직상 필요하여 유지 (필요시 제거)
 )
 
 data class ContestUpdateRequest(
