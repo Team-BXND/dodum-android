ed5194e715109bcc8e3832d0803692fd74b38264
parkhj2009 <eee0110010100@gmail.com>
2025-12-19 13:35:51 +0900

    하단 네비 바

--- app/src/main/java/com/example/dodum_android/MainActivity.kt
+++ app/src/main/java/com/example/dodum_android/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : ComponentActivity() {
             }
 
             if (tokenLoaded) {
-                AppNavHost(navController = navController)
+                MainScreen()
             }
         }
 
